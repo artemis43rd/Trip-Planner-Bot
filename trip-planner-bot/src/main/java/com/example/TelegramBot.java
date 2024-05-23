4b884b74c4687687b5ba8196b3d733c0e0b7da16
artemis43rd <43rd.artemis@mail.ru>
2024-05-23 23:12:45 +0300

    Bot v.1.0 - Repeater

---
+++ trip-planner-bot/src/main/java/com/example/TelegramBot.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("You said: " + messageText);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "trip_planner_java_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7138509311:AAGWbQ_RsRpnlI_-2vS0m6MbFvjci-PPwzo";
+    }
+}
